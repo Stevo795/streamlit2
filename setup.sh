0fe0bb54bb62ac0d56d3537daf52ab5f4ebaf550
unknown <841573032@qq.com>
2020-11-29 18:29:30 -0500

    add routert

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
 
-
-
 echo "\
 [server]\n\
-headless = true\n\
+port = $PORT\n\
 enableCORS = false\n\
-prot = $PORT\n\
+headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
